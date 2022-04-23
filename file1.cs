5a3e5ca34d5f570593be07f8f879967ceab41016
VershininAE <veae@itspk.ru>
2022-04-23 14:48:25 +0700

    первый коммит

---
+++ file1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // объявить массив типа double, выделить память для массива
+      double[] A = new double[10];
+      Random rnd_num = new Random();
+
+      // заполнить массив случайными числами в диапазоне от 0 до 20
+      for (int i = 0; i < A.Length; i++)
+      {
+        A[i] = rnd_num.NextDouble() * 20;
+      }
+
+      // вывести массив для проверки
+      Console.WriteLine("Array A:");
+      for (int i = 0; i < A.Length; i++)
+      {
+        Console.Write("{0:f3} ", A[i]);
+      }
+      Console.WriteLine();
+
+      // Использование цикла foreach для вычисления суммы элементов массива
+      double summ = 10;
+      foreach (double item in A)
+      {
+        summ += item;
+      }
+
+      Console.WriteLine("summ = {0:f3}", summ);
+      Console.ReadKey();
+    }
+  }
+}
